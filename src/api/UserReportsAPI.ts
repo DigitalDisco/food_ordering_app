985629dc4c3e706151c3dedb0aa4e930a408b345
Atlas Sandevi <62032234+DigitalDisco@users.noreply.github.com>
2023-05-31 10:47:28 +0200

    Update UserReportsAPI.ts

--- src/api/UserReportsAPI.ts
+++ src/api/UserReportsAPI.ts
@@ -7,42 +7,42 @@ import Avatar6 from "../images/UserCircles/Avatar6.png";
 
 export const UserReportsAPI = [
   {
-    image: Avatar1,
+    
     name: "Jon Treasach",
     description: "Spicy seasoned seafood noodles ",
     price: 125,
     status: 3,
   },
   {
-    image: Avatar2,
+    
     name: "Amir Luís",
     description: "Salted Pasta with mushroom sauce",
     price: 145,
     status: 1,
   },
   {
-    image: Avatar3,
+   
     name: "Oskar Florus",
     description: "Beef dumpling in hot and sour soup",
     price: 105,
     status: 2,
   },
   {
-    image: Avatar4,
+   
     name: "Cindy Shulamite",
     description: "Hot spicy fried rice with omelet",
     price: 45,
     status: 3,
   },
   {
-    image: Avatar5,
+  
     name: "Aquila Sinclair",
     description: "Hot spicy fried rice with omelet",
     price: 245,
     status: 3,
   },
   {
-    image: Avatar6,
+  
     name: "Tomte Nisse",
     description: "Hot spicy fried rice with omelet",
     price: 435,
